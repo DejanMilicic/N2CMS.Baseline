ddef7e933cd1de1732993c99bd5ea17f7307aeb5
Dejan Milicic <dejan.milicic@gmail.com>
2015-02-24 17:21:48 +0100

    Missing LanguageIntersection controller added

---
+++ content/Controllers/N2Pages/LanguageIntersectionController.cs.pp
@@ -0,0 +1,33 @@
+﻿
+namespace $rootnamespace$.Controllers.N2Pages
+{
+	using System.Web.Mvc;
+
+	using N2;
+	using N2.Web;
+	using N2.Web.Mvc;
+
+	using $rootnamespace$.Models.N2Pages;
+	using $rootnamespace$.N2Baseline;
+
+	[Controls(typeof(LanguageIntersection))]
+	public class LanguageIntersectionController : ContentController<LanguageIntersection>
+	{
+		public override ActionResult Index()
+		{
+			ContentItem language = Request.SelectLanguage(CurrentItem);
+			if (language != null)
+			{
+				if (language.Url.StartsWith("http"))
+					return Redirect(language.Url);
+
+				return ViewPage(language);
+			}
+
+			if (CurrentItem.RedirectUrl != CurrentItem.Url)
+				return Redirect(CurrentItem.RedirectUrl);
+
+			return View(CurrentItem);
+		}
+	}
+}
