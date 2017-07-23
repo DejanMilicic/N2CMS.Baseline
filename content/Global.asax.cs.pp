e2168844590f5ef136c7446c3190d4bd0d726034
Dejan Milicic <dejan.milicic@gmail.com>
2017-07-23 22:58:30 +0200

    ISaved content action

--- content/Global.asax.cs.pp
+++ content/Global.asax.cs.pp
@@ -3,6 +3,8 @@ namespace $rootnamespace$
 {
 	using System.Web.Mvc;
 	using System.Web.Routing;
+	using $rootnamespace$.N2Baseline.Interfaces.ContentItemActions;
+    using System;
 
 	public class Global : System.Web.HttpApplication
 	{
@@ -10,6 +12,22 @@ namespace $rootnamespace$
 		{
 			AreaRegistration.RegisterAllAreas();
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			N2.Context.Current.Persister.ItemSaved += Persister_ItemSaved;
 		}
+
+		protected void Application_End(object sender, EventArgs e)
+	    {
+	        N2.Context.Current.Persister.ItemSaved -= Persister_ItemSaved;
+	    }
+
+	    private void Persister_ItemSaved(object sender, N2.ItemEventArgs e)
+	    {
+	        // check if item is not deleted
+	        if (e.AffectedItem is ISaved && e.AffectedItem.State != N2.ContentState.Deleted)
+	        {
+	            (e.AffectedItem as ISaved).OnSaved(e.AffectedItem, e);
+	        }
+	    }
 	}
 }
---
+++ content/N2Baseline/Interfaces/ContentItemActions/ISaved.cs.pp
@@ -0,0 +1,12 @@
+﻿using N2;
+
+namespace $rootnamespace$.N2Baseline.Interfaces.ContentItemActions
+{
+    /// <summary>
+    /// Action that is executed after content item has been saved by N2
+    /// </summary>
+    interface ISaved
+    {
+        void OnSaved(ContentItem item, ItemEventArgs e);
+    }
+}
